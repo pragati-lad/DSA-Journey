d6be7ad155cf01b525475708d26b20a884254f19
pragati lad <pragatilad81@gmail.com>
2025-05-06 23:55:53 +0530

    Create matching-same-text-again-again.py

---
+++ HackerRank/regx/matching-same-text-again-again.py
@@ -0,0 +1,5 @@
+Regex_Pattern = r"^(([a-z])(\w)([\n\t\f\s\r])(\W)(\d)(\D)([A-Z])([a-zA-Z])([aeiouAEIOU])(\S))\1$"	# Do not delete 'r'.
+
+# '^([a-z])(\w)([\n\t\f\s\r])(\W)(\d)(\D)([A-Z])([a-zA-Z])
+# ([aeiouAEIOU])(\S)\1\2\3\4\5\6\7\8\9\10$'
+import re
