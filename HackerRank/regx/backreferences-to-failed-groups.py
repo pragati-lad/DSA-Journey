544c7f38c77cd36c9339340bf0fe3046e8f7dd86
pragati lad <pragatilad81@gmail.com>
2025-05-07 14:28:47 +0530

    Create backreferences-to-failed-groups.py

---
+++ HackerRank/regx/backreferences-to-failed-groups.py
@@ -0,0 +1,7 @@
+Regex_Pattern = r"^(\d{2})(-?)(\d{2})\2(\d{2})\2(\d{2})$"	# Do not delete 'r'.
+                        ##either match - or nothing
+                        
+
+#^\d{8}$|^(\d{2}-){3}\d{2}$
+# ^(\d{2}-{1}){4}$ -------- xx-xx-xx-xx-
+import re
