19958cd6eb32ffa616da153211dd627c2a30ede0
pragati lad <pragatilad81@gmail.com>
2025-05-07 14:47:02 +0530

    Create branch-reset-groups.pl

---
+++ HackerRank/regx/branch-reset-groups.pl
@@ -0,0 +1,3 @@
+$Regex_Pattern = '^\d{2}(?|(---)|(-)|(.)|(:))(\d{2})\1(\d{2})\1(\d{2})$'; # captured
+                         # (?|---|-|.|:)  --- no captturing of groups 
+$Test_String = <STDIN> ;
